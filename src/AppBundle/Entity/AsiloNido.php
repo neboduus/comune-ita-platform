2753cef2ed8318ea2aa436dc0fdf6062b0cbb91e
Luca Realdi <lr@opencontent.it>
2016-08-28 16:00:56 +0200

    minor bugfix

--- src/AppBundle/Entity/AsiloNido.php
+++ src/AppBundle/Entity/AsiloNido.php
@@ -29,7 +29,7 @@ class AsiloNido
 
     /**
      * @var string
-     * @ORM\Column(type="string" , nullable=true)
+     * @ORM\Column(type="text" , nullable=true)
      */
     private $schedaInformativa;
 
