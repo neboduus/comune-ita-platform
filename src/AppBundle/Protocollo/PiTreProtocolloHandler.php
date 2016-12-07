2c350b05c1e045596b6c3a1c18ea11e6dfd7ff36
Luca Realdi <luca.realdi@opencontent.it>
2016-12-07 14:36:14 +0100

    finetuning chiamate pitre

--- src/AppBundle/Protocollo/PiTreProtocolloHandler.php
+++ src/AppBundle/Protocollo/PiTreProtocolloHandler.php
@@ -28,14 +28,16 @@ class PiTreProtocolloHandler implements ProtocolloHandlerInterface
     public function sendPraticaToProtocollo(Pratica $pratica)
     {
         $parameters = $this->getParameters($pratica);
-        $response = $this->client->request('POST', '/createDocumentAndAddInProject', ['json' => $parameters->all()]);
+        $parameters->set('method', 'createDocumentAndAddInProject');
+        $queryString = http_build_query($parameters->all());
+        $response = $this->client->get('?' . $queryString);
         $responseData = new PiTreResponseData((array)json_decode((string)$response->getBody(), true));
 
         if ($responseData->getStatus() == 'success') {
-            $pratica->setNumeroFascicolo($responseData->getIdDoc());
+            $pratica->setIdDocumentoProtocollo($responseData->getIdDoc());
             $pratica->setNumeroProtocollo($responseData->getNProt());
         } else {
-            throw new ResponseErrorException($responseData);
+            throw new ResponseErrorException($responseData . ' on query ' . $queryString);
         }
     }
 
@@ -48,7 +50,9 @@ class PiTreProtocolloHandler implements ProtocolloHandlerInterface
     public function sendAllegatoToProtocollo(Pratica $pratica, AllegatoInterface $allegato)
     {
         $parameters = $this->getParameters($pratica, $allegato);
-        $response = $this->client->request('POST', '/uploadFileToDocument', ['json' => $parameters->all()]);
+        $parameters->set('method', 'uploadFileToDocument');
+        $queryString = http_build_query($parameters->all());
+        $response = $this->client->get('?' . $queryString);
         $responseData = new PiTreResponseData((array)json_decode((string)$response->getBody(), true));
         if ($responseData->getStatus() == 'success') {
             $pratica->addNumeroDiProtocollo([
@@ -56,7 +60,7 @@ class PiTreProtocolloHandler implements ProtocolloHandlerInterface
                 'protocollo' => $responseData->getIdDoc(),
             ]);
         } else {
-            throw new ResponseErrorException($responseData);
+            throw new ResponseErrorException($responseData . ' on query ' . $queryString);
         }
     }
 
@@ -68,17 +72,16 @@ class PiTreProtocolloHandler implements ProtocolloHandlerInterface
         $parameters = new PiTreProtocolloParameters($parameters);
 
         if ($allegato instanceof AllegatoInterface){
-            $parameters->setDocumentId($pratica->getNumeroFascicolo());
-            $parameters->setFilePath($allegato->getFile()->getPath());
+            $parameters->setDocumentId($pratica->getIdDocumentoProtocollo());
+            $parameters->setFilePath($allegato->getFile()->getPathname());
             $parameters->setAttachmentDescription($allegato->getDescription());
         }else{
             /** @var ModuloCompilato $moduloCompilato */
             $moduloCompilato = $pratica->getModuliCompilati()->first();
-
             $parameters->setProjectDescription($pratica->getServizio()->getName() . ' ' . $pratica->getUser()->getFullName());
             $parameters->setDocumentDescription($moduloCompilato->getDescription());
             $parameters->setDocumentObj($pratica->getServizio()->getName());
-            $parameters->setFilePath($moduloCompilato->getFile()->getPath());
+            $parameters->setFilePath($moduloCompilato->getFile()->getPathname());
         }
 
         return $parameters;
