38c8467c18d50862e1a95de15021bbcd2c79c0f9
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2018-05-02 13:11:33 +0200

    Modifiche comando creazione servizio

--- src/AppBundle/Command/ServizioCreateCommand.php
+++ src/AppBundle/Command/ServizioCreateCommand.php
@@ -6,37 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Cache\Exception\InvalidArgumentException;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * Class ServizioCreateCommand
  */
 class ServizioCreateCommand extends ContainerAwareCommand
 {
-
-    const HELP = <<<HELP
-Crea un record nella tabella servizi, lo marca come disabilitato, va abilitato a mano una volta che si è creato il flow corrispondente.
-I valori necessari sono
-Lo slug del servizio (sarà usato anche per le rotte)
-Il nome del servizio (testo libero)
-L'FQCN della classe persistita che rappresenta il servizio (quella in /Entity/...)
-Il nome del servizio che rappresenta il flow
-
-FQCN e flow possono essere creati successivamente, ma finché non ci sono il sistema darà una serie di errori e sarà inutilizzabile (nota utile di fatto solo per lo sviluppo)
-HELP;
-
     protected function configure()
     {
         $this
             ->setName('ocsdc:crea-servizio')
-            ->setDescription('Crea un record nella tabella servizi')
-            ->setHelp(self::HELP)
-            ->addArgument('slug', InputArgument::REQUIRED)
-            ->addArgument('name', InputArgument::REQUIRED)
-            ->addArgument('fcqn', InputArgument::REQUIRED)
-            ->addArgument('flow', InputArgument::REQUIRED);
+            ->setDescription('Crea un record nella tabella servizi');
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    /*protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln([
             'Crea Servizi',
@@ -82,5 +68,133 @@ HELP;
         $output->writeln("\n6 - Crea il template Resources/views/Pratiche/summary/{$className}.html.twig per la visualizzazione dei sommari");
         $output->writeln("\n7 - Crea il template Resources/views/Pratiche/pdf/{$className}.html.twig per il render dei pdf");
         $output->writeln("\n8 - Crea la classe di test Tests\\Flows\\{$className}Test");
+    }*/
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $helper = $this->getHelper('question');
+
+        // Name
+        $question = new Question('Inserisci il nome del Servizio: ', '');
+        $question->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Inserimento obbligatoorio'
+                );
+            }
+            return $answer;
+        });
+        $name = $helper->ask($input, $output, $question);
+
+        // Enti
+        // Lo imposto direttamente
+        /*$codiciMeccanograficiEnti = array();
+        $question = new Question('Inserisci Codice Meccanografico del Servizio (in caso di più valori separare con ##): ', $this->getContainer()->getParameter('codice_meccanografico'));
+        $question->setValidator(function ($answer) {
+            $codiciMeccanograficiEnti = explode('##', $answer);
+            if ($this->getContainer()->hasParameter('codice_meccanografico') && !in_array( $this->getContainer()->getParameter('codice_meccanografico'), $codiciMeccanograficiEnti)) {
+                throw new \RuntimeException(
+                    'Codice meccanografico non presente per questa istanza'
+                );
+            }
+            return $answer;
+        });
+        $codiciEnti = $helper->ask($input, $output, $question);*/
+
+        // Handler
+        $question = new Question('Inserisci l\'handler del Servizio: ', '');
+        $question->setValidator(function ($answer) {
+            if (!is_string($answer) || empty($answer)) {
+                throw new \RuntimeException(
+                    'Inserimento obbligatoorio'
+                );
+            }
+            return $answer;
+        });
+        $handler = $helper->ask($input, $output, $question);
+
+        // Area
+        $question = new Question('Inserisci Area del Servizio: ', '');
+        $question->setValidator(function ($answer) {
+            if (empty($answer)) {
+                throw new \RuntimeException(
+                    'Inserimento obbligatoorio'
+                );
+            }
+            if (!is_numeric($answer)) {
+                throw new \RuntimeException(
+                    'Sono ammessi solo numeri interi'
+                );
+            }
+            return $answer;
+        });
+        $area = $helper->ask($input, $output, $question);
+
+        // Description
+        $question = new Question('Inserisci la descrizione del Servizio: ', '');
+        $description = $helper->ask($input, $output, $question);
+
+        // Istruzioni
+        $question = new Question('Inserisci le istruzioni del Servizio: ', '');
+        $istruzioni = $helper->ask($input, $output, $question);
+
+        // fcqn
+        $question = new Question('Inserisci fcqn del Servizio: ', '');
+        $fcqn = $helper->ask($input, $output, $question);
+
+        // flow
+        $question = new Question('Inserisci flow del Servizio: ', '');
+        $flow = $helper->ask($input, $output, $question);
+
+        // flow operatore
+        $question = new Question('Inserisci flow operatore del Servizio: ', '');
+        $flowOperator = $helper->ask($input, $output, $question);
+
+
+
+        $manager      = $this->getContainer()->get('doctrine')->getManager();
+        $serviziRepo  = $manager->getRepository('AppBundle:Servizio');
+        $categoryRepo = $manager->getRepository('AppBundle:Categoria');
+
+        $servizio = $serviziRepo->findOneByName($name);
+        if (!$servizio instanceof Servizio) {
+
+            $servizio = new Servizio();
+            $servizio
+                ->setName($name)
+                ->setHandler($handler)
+                ->setDescription($description)
+                ->setTestoIstruzioni($istruzioni)
+                ->setPraticaFCQN($fcqn)
+                ->setPraticaFlowServiceName($flow)
+                ->setPraticaFlowOperatoreServiceName($flowOperator)
+                ->setStatus(1);
+
+            $area = $categoryRepo->findOneByTreeId($area);
+            if ($area instanceof Categoria) {
+                $servizio->setArea($area);
+            }
+
+            $manager->persist($servizio);
+
+            $codiciMeccanograficiEnti = $this->getContainer()->getParameter('codice_meccanografico');
+            $enti = $manager->getRepository('AppBundle:Ente')->findBy(['codiceMeccanografico' => $codiciMeccanograficiEnti]);
+            foreach ($enti as $ente) {
+                $erogatore = new Erogatore();
+                $erogatore->setName('Erogatore di ' . $servizio->getName() . ' per ' . $ente->getName());
+                $erogatore->addEnte($ente);
+                $manager->persist($erogatore);
+                $servizio->activateForErogatore($erogatore);
+            }
+            $manager->flush();
+
+
+            $output->writeln("<info>Servizio creato correttamente</info>");
+
+        } else {
+            $output->writeln("<error>Servizio già presente, non verrà eseguita nessuan azione</error>");
+        }
+
+
     }
 }
