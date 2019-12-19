175a3ac5044216ce8d30415b5627dc1155c53706
lorello <lorello@openweb.it>
2019-12-19 14:07:20 +0100

    Improvement in dev environment

--- compose_conf/php/init-demo.sh
+++ compose_conf/php/init-demo.sh
@@ -2,6 +2,8 @@
 
 if [[ $ENV == 'DEMO' ]]; then
 
+	echo "==> ENV=DEMO, loading doctrine fixtures to create a default environment"
+
 	php bin/console --no-interaction doctrine:fixtures:load
 
 	for instance in $(ls -1 app/config/|grep comune); do
