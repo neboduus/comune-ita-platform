fd9fbc3b85b9783cf08c07dad07a5ab207fcabdb
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2020-01-11 18:21:26 +0100

    Fixed parameters.yaml Removed duplicated migrations file

--- compose_conf/php/migrate-db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-php bin/console --no-interaction doctrine:migrations:migrate
-
-for instance in $(./bin/tenants); do
-   php bin/console --no-interaction --instance $instance doctrine:migrations:migrate
-done
