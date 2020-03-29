b0e2f7b3df883ef6202ffdc5ff7e1e8c27565dac
lorello <lorello@openweb.it>
2020-03-29 19:47:54 +0200

    Minor change: php command is not required if php script has shebang

--- compose_conf/php/init.d/init-20-db.sh
+++ compose_conf/php/init.d/init-20-db.sh
@@ -3,8 +3,8 @@
 DB_HOST=${DB_HOST:-postgres}
 DB_PORT=${DB_PORT:-5432}
 
-wait-for-it ${DB_HOST}:${DB_PORT} --timeout=0 --strict -- php bin/console --no-interaction doctrine:migrations:migrate
+wait-for-it ${DB_HOST}:${DB_PORT} --timeout=0 --strict -- bin/console --no-interaction doctrine:migrations:migrate
 
 for instance in $(./bin/tenants); do
-   wait-for-it ${DB_HOST}:${DB_PORT} --timeout=0 --strict -- php bin/console --no-interaction --instance ${instance} doctrine:migrations:migrate
+   wait-for-it ${DB_HOST}:${DB_PORT} --timeout=0 --strict -- bin/console --no-interaction --instance ${instance} doctrine:migrations:migrate
 done
