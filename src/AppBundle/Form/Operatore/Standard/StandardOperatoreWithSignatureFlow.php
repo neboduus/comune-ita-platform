5651fa8e5abbcb9814edba5452c221b39060c878
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2020-05-12 17:04:02 +0000

    Removed signature from standard operator flow

--- src/AppBundle/Form/Operatore/Standard/StandardOperatoreWithSignatureFlow.php
+++ src/AppBundle/Form/Operatore/Standard/StandardOperatoreWithSignatureFlow.php
@@ -9,9 +9,11 @@ use AppBundle\Form\Operatore\Base\UploadRispostaOperatoreType;
 /**
  * Class StandardOperatoreFlow
  */
-class StandardOperatoreFlow extends PraticaOperatoreFlow
+class StandardOperatoreWithSignatureFlow extends PraticaOperatoreFlow
 {
     const STEP_APPROVA_O_RIGETTA = 1;
+    const STEP_ALLEGA_RISPOSTA_FIRMATA = 2;
+
     protected $allowDynamicStepNavigation = true;
 
     protected function loadStepsConfig()
@@ -20,7 +22,12 @@ class StandardOperatoreFlow extends PraticaOperatoreFlow
             self::STEP_APPROVA_O_RIGETTA => array(
                 'label' => 'operatori.approva',
                 'form_type' => ApprovaORigettaType::class
+            ),
+            self::STEP_ALLEGA_RISPOSTA_FIRMATA => array(
+                'label' => 'operatori.allega_risposta_firmata',
+                'form_type' => UploadRispostaOperatoreType::class
             )
         );
     }
+
 }
