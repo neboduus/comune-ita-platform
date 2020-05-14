08c1940332d7a6a92bc829551788b28a15cbdcfc
Luca Realdi <luca.realdi@opencontent.it>
2020-05-14 14:55:58 +0000

    Validate cps user fields against formio schema before inject in form data

--- src/AppBundle/Form/FormIO/FormIOFlow.php
+++ src/AppBundle/Form/FormIO/FormIOFlow.php
@@ -4,24 +4,24 @@ namespace AppBundle\Form\FormIO;
 
 use AppBundle\Entity\Pratica;
 use AppBundle\Entity\Servizio;
-use AppBundle\Form\Base\AccettazioneIstruzioniType;
-use AppBundle\Form\Base\DatiRichiedenteType;
+use AppBundle\Form\Base\PaymentGatewayType;
 use AppBundle\Form\Base\PraticaFlow;
 use AppBundle\Form\Base\RecaptchaType;
+use AppBundle\Form\Base\SelectPaymentGatewayType;
 use AppBundle\Form\Base\SelezionaEnteType;
 use AppBundle\Form\Base\SummaryType;
-use AppBundle\Form\Scia\PraticaEdiliziaVincoliType;
 use Craue\FormFlowBundle\Form\FormFlowInterface;
-use AppBundle\Form\Base\SelectPaymentGatewayType;
-use AppBundle\Form\Base\PaymentGatewayType;
 
 class FormIOFlow extends PraticaFlow
 {
-  const STEP_DATI_RICHIEDENTE = 1;
+
+  const STEP_SELEZIONA_ENTE = 1;
+
   const STEP_MODULO_FORMIO = 2;
 
-
   protected $allowDynamicStepNavigation = true;
+
+  protected $revalidatePreviousSteps = true;
 
   protected function loadStepsConfig()
   {
@@ -35,14 +35,14 @@ class FormIOFlow extends PraticaFlow
         'skip' => function ($estimatedCurrentStepNumber, FormFlowInterface $flow) {
           return $flow->getFormData()->getStatus() != Pratica::STATUS_DRAFT;
         },
-      )
+      ),
     );
 
     // Mostro lo step del'ente solo se è necesario
     if ($pratica->getEnte() == null && $this->prefix == null) {
       $steps [self::STEP_SELEZIONA_ENTE] = array(
         'label' => 'steps.common.seleziona_ente.label',
-        'form_type' => SelezionaEnteType::class
+        'form_type' => SelezionaEnteType::class,
       );
     }
     ksort($steps);
@@ -65,24 +65,24 @@ class FormIOFlow extends PraticaFlow
       );
       $steps[] = array(
         'label' => 'steps.common.payment_gateway.label',
-        'form_type' => PaymentGatewayType::class
+        'form_type' => PaymentGatewayType::class,
       );
       $steps[] = array(
-        'label' => 'Verifica pagamento'
+        'label' => 'Verifica pagamento',
       );
     } else {
       // Step conferma
-      if ( $pratica->getServizio()->getAccessLevel() > Servizio::ACCESS_LEVEL_ANONYMOUS) {
+      if ($pratica->getServizio()->getAccessLevel() > Servizio::ACCESS_LEVEL_ANONYMOUS) {
         $steps[] = array(
-          'label' => 'steps.common.conferma.label'
+          'label' => 'steps.common.conferma.label',
         );
       } else {
         $steps[] = array(
-          'label'     => 'steps.common.conferma.label',
+          'label' => 'steps.common.conferma.label',
           'form_type' => RecaptchaType::class,
           'form_options' => [
-            'validation_groups' => 'recaptcha'
-          ]
+            'validation_groups' => 'recaptcha',
+          ],
         );
       }
 
