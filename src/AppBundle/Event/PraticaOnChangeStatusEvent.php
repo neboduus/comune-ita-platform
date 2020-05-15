0a7e2f30b5f7c2c3c20ef15bfbea9f5b36372621
Luca Realdi <lr@opencontent.it>
2020-05-15 03:26:27 +0200

    Log browser applicant data per pratica in xiidea_easy_audit

--- src/AppBundle/Event/PraticaOnChangeStatusEvent.php
+++ src/AppBundle/Event/PraticaOnChangeStatusEvent.php
@@ -7,37 +7,51 @@ use Symfony\Component\EventDispatcher\Event;
 
 class PraticaOnChangeStatusEvent extends Event
 {
-    /**
-     * @var Pratica
-     */
-    private $pratica;
+  /**
+   * @var Pratica
+   */
+  private $pratica;
 
-    /**
-     * @var string
-     */
-    private $newStateIdentifier;
+  /**
+   * @var string
+   */
+  private $newStateIdentifier;
+
+  /**
+   * @var string
+   */
+  private $oldStateIdentifier;
 
 
-    public function __construct(Pratica $pratica, $newStateIdentifier)
-    {
-        $this->pratica = $pratica;
-        $this->newStateIdentifier = $newStateIdentifier;
-    }
+  public function __construct(Pratica $pratica, $newStateIdentifier, $oldStateIdentifier)
+  {
+    $this->pratica = $pratica;
+    $this->newStateIdentifier = $newStateIdentifier;
+    $this->oldStateIdentifier = $oldStateIdentifier;
+  }
 
-    /**
-     * @return Pratica
-     */
-    public function getPratica()
-    {
-        return $this->pratica;
-    }
+  /**
+   * @return Pratica
+   */
+  public function getPratica()
+  {
+    return $this->pratica;
+  }
 
-    /**
-     * @return string
-     */
-    public function getNewStateIdentifier()
-    {
-        return $this->newStateIdentifier;
-    }
+  /**
+   * @return string
+   */
+  public function getNewStateIdentifier()
+  {
+    return $this->newStateIdentifier;
+  }
+
+  /**
+   * @return string
+   */
+  public function getOldStateIdentifier()
+  {
+    return $this->oldStateIdentifier;
+  }
 
 }
