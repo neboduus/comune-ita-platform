98d429841ce6f0da15f0f155a6091f8535c1a390
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2020-10-16 23:03:30 +0200

    Fixed apache conf, dockerfile, moved migrations

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 Encore
 // directory where compiled assets will be stored
-  .setOutputPath('web/build/')
+  .setOutputPath('public/build/')
   // public path used by the web server to access the output path
   .setPublicPath('/build')
   // only needed for CDN's or sub-directory deploy
