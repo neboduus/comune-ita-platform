f627256bd4cb31a7ef40fd1a292011a39c1edcfe
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2020-10-18 16:28:18 +0200

    Other fixes

--- bin/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull
-./bin/dockerized_css
-docker-compose build
-#OCSDC PATH is assumed to be ~
-docker run --rm -v ~/ocsdc/web:/docroot/web -it ocsdc_ocsdc:latest /docroot/bin/console assets:install
-docker-compose up -d
