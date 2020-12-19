ed5548ff0832780cb023ab9e225d4a97cabeaa8c
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2020-12-19 13:23:58 +0100

    Fixed migrations

--- migrations/Version20201116220428.php
+++ migrations/Version20201116220428.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Application\Migrations;
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -10,19 +12,20 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201116220428 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+      return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('ALTER TABLE ente ADD mailers JSON DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('ALTER TABLE ente DROP mailers');
     }
 }
--- migrations/Version20201117173558.php
+++ migrations/Version20201117173558.php
@@ -1,6 +1,8 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace Application\Migrations;
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -10,19 +12,20 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20201117173558 extends AbstractMigration
 {
+    public function getDescription() : string
+    {
+      return '';
+    }
+
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('ALTER TABLE pratica ALTER COLUMN richiedente_cap_residenza TYPE VARCHAR(10)');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
-
         $this->addSql('ALTER TABLE pratica ALTER COLUMN richiedente_cap_residenza TYPE INT USING richiedente_cap_residenza::integer');
     }
 }
