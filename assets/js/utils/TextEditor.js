00540c7a4c6354dae142b59e598fce06be6acd5f
Sabato <sabatogalasso90@gmail.com>
2021-02-23 20:36:02 +0000

    fix summernote formatting

--- assets/js/utils/TextEditor.js
+++ assets/js/utils/TextEditor.js
@@ -1,9 +1,33 @@
 import 'summernote';
 import 'summernote/dist/summernote-bs4.css';
-import 'summernote-cleaner';
 export class TextEditor {
 
   static init(callback) {
+    function CleanPastedHTML(input) {
+      // 1. remove line breaks / Mso classes
+      var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
+      var output = input.replace(stringStripper, ' ');
+      // 2. strip Word generated HTML comments
+      var commentSripper = new RegExp('<!--(.*?)-->','g');
+      var output = output.replace(commentSripper, '');
+      var tagStripper = new RegExp('<(/)*(meta|link|span|\\?xml:|st1:|o:|font)(.*?)>','gi');
+      // 3. remove tags leave content if any
+      output = output.replace(tagStripper, '');
+      // 4. Remove everything in between and including tags '<style(.)style(.)>'
+      var badTags = ['style', 'script','applet','embed','noframes','noscript','pre'];
+
+      for (var i=0; i< badTags.length; i++) {
+        tagStripper = new RegExp('<'+badTags[i]+'.*?'+badTags[i]+'(.*?)>', 'gi');
+        output = output.replace(tagStripper, '');
+      }
+      // 5. remove attributes ' style="..."'
+      var badAttributes = ['style', 'start','pre'];
+      for (var i=0; i< badAttributes.length; i++) {
+        var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"','gi');
+        output = output.replace(attributeStripper, '');
+      }
+      return output;
+    }
     $('textarea').summernote({
       toolbar: [
         ['style', ['bold', 'italic', 'underline', 'clear']],
@@ -12,17 +36,13 @@ export class TextEditor {
         ['view', ['codeview']],
       ],
       fontNames: ['Titillium Web', 'Geneva', 'Tahoma', 'sans-serif'],
-      cleaner:{
-        action: 'both', // both|button|paste 'button' only cleans via toolbar button, 'paste' only clean when pasting content, both does both options.
-        keepHtml: true, // Remove all Html formats
-        keepOnlyTags: ['<p>', '<br>', '<ul>', '<li>', '<b>', '<strong>','<i>', '<a>'], // If keepHtml is true, remove all tags except these
-        keepClasses: false, // Remove Classes
-        limitChars: 2000, // 0/false|# 0/false disables option
-        limitDisplay: 'both', // text|html|both
-      },
       callbacks: callback
-    });
+    }).on('summernote.paste', function(e, ne) {
+      e.currentTarget.innerHTML = CleanPastedHTML(e.currentTarget.innerHTML)
+    })
   }
+
+
 }
 
 
