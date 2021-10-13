40c5a6180c7ba18240fe622ee342970f02d69892
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2021-10-13 16:53:31 +0200

    Fixed add numero to protocollo in predispose protocol

--- src/AppBundle/Protocollo/PiTreProtocolloHandler.php
+++ src/AppBundle/Protocollo/PiTreProtocolloHandler.php
@@ -106,7 +106,7 @@ class PiTreProtocolloHandler implements ProtocolloHandlerInterface, PredisposedP
     if ($responseData->getStatus() == 'success') {
       $attachment->setNumeroProtocollo($responseData->getNProt());
       $pratica->addNumeroDiProtocollo([
-        'id' => $attachment->getIdDocumentoProtocollo(),
+        'id' => $attachment->getId(),
         'protocollo' => $attachment->getIdDocumentoProtocollo(),
       ]);
     } else {
