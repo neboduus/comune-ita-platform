3124d70a352f442fda7efd5d38ab6240ab938dc0
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2021-11-09 17:57:20 +0100

    Release 1.10.0-rc.5

--- src/AppBundle/Services/FileService.php
+++ src/AppBundle/Services/FileService.php
@@ -90,11 +90,12 @@ class FileService
     $filePath = $mapping->getDirectoryNamer()->directoryName($allegato, $mapping);
     if ($absolute) {
       $prefix = str_replace('/', '', $mapping->getUriPrefix());
-      $filenameWithPath = sprintf('%s%s/%s', $prefix, $filePath, $allegato->getFileName());
+      $filenameWithPath = sprintf('%s/%s/%s', $prefix, $filePath, $allegato->getFileName());
     } else {
       $filenameWithPath = sprintf('%s/%s', $filePath, $allegato->getFileName());
     }
-
+    // Todo: correggere impostazioni e testare upload
+    $filenameWithPath = str_replace('//', '/', $filenameWithPath);
     return $filenameWithPath;
   }
 
