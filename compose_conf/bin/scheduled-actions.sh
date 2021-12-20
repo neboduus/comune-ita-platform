1c2fb8b8a6fc24f31037e18022dc8774e2341d89
Lorenzo Salvadorini <lorenzo.salvadorini@opencontent.it>
2021-12-20 15:08:46 +0000

    Update scheduled actions with password expiration, meeting draft craetion and payments draft creation

--- compose_conf/bin/scheduled-actions.sh
+++ compose_conf/bin/scheduled-actions.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 for instance in $(./bin/tenants); do
-   echo "Execute scheduled actions for tenant: $instance"
-   php bin/console --no-interaction --instance $instance ocsdc:scheduled_action:execute
+  echo "Execute scheduled actions for tenant: $instance"
+  php bin/console --no-interaction --instance $instance ocsdc:scheduled_action:execute -o 60 --max-retry 10 --count 50
+  echo "Disable users for tenant: $instance"
+  php bin/console --no-interaction --instance $instance ocsdc:user-secure:execute
+  echo "Delete draft meetings for tenant: $instance"
+  php bin/console --no-interaction --instance $instance ocsdc:delete-draft-meetings
+  echo "Create subscription payments draft for tenant: $instance"
+  php bin/console --no-interaction --instance $instance ocsdc:create_subscription_payment_drafts
 done
