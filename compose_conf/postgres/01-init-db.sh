f935d74e5bafc9b35f3ba2f9572e2b3301b512c5
Lorenzo Salvadorini <lorenzo.salvadorini@opencontent.it>
2022-04-11 14:32:35 +0000

    Removed references to sdc_multi

--- compose_conf/postgres/01-init-db.sh
+++ compose_conf/postgres/01-init-db.sh
@@ -6,12 +6,6 @@ set -e
 psql -v ON_ERROR_STOP=0 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         CREATE USER $DB_USER WITH PASSWORD '$DB_PASSWORD';
 
-        CREATE DATABASE sdc_multi;
-        GRANT ALL PRIVILEGES ON DATABASE sdc_multi TO $DB_USER;
-
-        CREATE DATABASE sdc_vallelaghi;
-        GRANT ALL PRIVILEGES ON DATABASE sdc_vallelaghi TO $DB_USER;
-
         CREATE DATABASE sdc_bugliano;
         GRANT ALL PRIVILEGES ON DATABASE sdc_bugliano TO $DB_USER;
 EOSQL
