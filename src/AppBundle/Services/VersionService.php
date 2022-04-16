915766ed81b74b521976db426052ac266b3f8a5b
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-04-16 08:57:51 +0000

    Added parameter to version service

--- src/AppBundle/Services/VersionService.php
+++ src/AppBundle/Services/VersionService.php
@@ -5,18 +5,28 @@ namespace AppBundle\Services;
 class VersionService
 {
 
-    private $version;
+  private $version;
 
-    public function getVersion()
-    {
-        if ($this->version == null)
-        {
-          if (is_file('../web/VERSION')) {
-            $this->version = file_get_contents('../web/VERSION');
-          } else {
-            $this->version = 'Unknown';
-          }
-        }
-        return $this->version;
+  /**
+   * @param $version
+   */
+  public function __construct($version)
+  {
+    $this->version = $version;
+  }
+
+  /**
+   * @return false|string
+   */
+  public function getVersion()
+  {
+    if ($this->version == null) {
+      if (is_file('../web/VERSION')) {
+        $this->version = file_get_contents('../web/VERSION');
+      } else {
+        $this->version = 'Unknown';
+      }
     }
+    return $this->version;
+  }
 }
