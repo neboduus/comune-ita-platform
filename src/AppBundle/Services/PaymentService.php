f8074b830f580d7280ed1136b9049ebdcb3aa93c
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-05-20 15:22:11 +0200

    Fixed fetch of payments, added remote id filter and empty response

--- src/AppBundle/Services/PaymentService.php
+++ src/AppBundle/Services/PaymentService.php
@@ -170,16 +170,22 @@ class PaymentService
   }
 
   /**
-   * @param Pratica $application
+   * @param $remoteId
    * @return array
    * @throws \Exception
    */
-  public function getPayments(Pratica $application): array
+  public function getPayments($remoteId): array
   {
     $data = [];
     $curl = curl_init();
+
+    $query = "SELECT ID, USER_ID, TYPE, TENANT_ID, SERVICE_ID, CREATED_AT, UPDATED_AT, STATUS, REASON, REMOTE_ID, PAYMENT_TRANSACTION_ID, PAYMENT_PAID_AT, PAYMENT_EXPIRE_AT, PAYMENT_AMOUNT, PAYMENT_CURRENCY, PAYMENT_NOTICE_CODE, PAYMENT_IUD, PAYMENT_IUV, ONLINE_PAYMENT_BEGIN_URL, ONLINE_PAYMENT_BEGIN_LAST_OPENED_AT, ONLINE_PAYMENT_BEGIN_METHOD, ONLINE_PAYMENT_LANDING_URL, ONLINE_PAYMENT_LANDING_LAST_OPENED_AT, ONLINE_PAYMENT_LANDING_METHOD, OFFLINE_PAYMENT_URL, OFFLINE_PAYMENT_LAST_OPENED_AT, OFFLINE_PAYMENT_METHOD, RECEIPT_URL, RECEIPT_LAST_OPENED_AT, RECEIPT_METHOD, UPDATE_URL, UPDATE_LAST_CHECK_AT, UPDATE_NEXT_CHECK_AT, UPDATE_METHOD, PAYER_TYPE, PAYER_TAX_IDENTIFICATION_NUMBER, PAYER_NAME, PAYER_FAMILY_NAME, PAYER_STREET_NAME, BUILDING_NUMBER, POSTAL_CODE, TOWN_NAME, COUNTRY_SUBDIVISION, COUNTRY, EMAIL, EVENT_ID, EVENT_VERSION, EVENT_CREATED_AT, APP_ID FROM payments_detail";
+    if ($remoteId) {
+      $query .=  " WHERE remote_id = '{$remoteId}'";
+    }
+
     $payload = [
-      'ksql' => "SELECT * FROM payments_detail WHERE remote_id = '{$application->getId()}';",
+      'ksql' => $query . ';',
     ];
 
     $url = $this->ksqlDBUrl . '/query';
@@ -211,30 +217,82 @@ class PaymentService
         return $data;
       }
 
-      $data = [
-        "id" => $responseData[1]['row']['columns'][0],
-        "reason" => $responseData[1]['row']['columns'][1],
-        "status" => $responseData[1]['row']['columns'][2],
-        "created_at" => $responseData[1]['row']['columns'][3],
-        "updated_at" => $responseData[1]['row']['columns'][4],
-      ];
+      // Elimino la riga con l'header
+      unset($responseData[0]);
 
-      if (strtoupper($data['status']) === 'PAYMENT_PENDING') {
-        $data['links'] = [
-          'online_payment_begin' => [
-            'url' => $responseData[1]['row']['columns'][5],
-            'method' => $responseData[1]['row']['columns'][6],
+      foreach ($responseData as $v) {
+        $payment = [
+          'id' => $v['row']['columns'][0],
+          'user_id' => $v['row']['columns'][1],
+          'type' => $v['row']['columns'][2],
+          'tenant_id' => $v['row']['columns'][3],
+          'service_id' => $v['row']['columns'][4],
+          'created_at' => $v['row']['columns'][5],
+          'updated_at' => $v['row']['columns'][6],
+          'status' => $v['row']['columns'][7],
+          'reason' => $v['row']['columns'][8],
+          'remote_id' => $v['row']['columns'][9],
+          'payment' => [
+            'transaction_id' => $v['row']['columns'][10],
+            'paid_at' => $v['row']['columns'][11],
+            'expire_at' => $v['row']['columns'][12],
+            'amount' => $v['row']['columns'][13],
+            'currency' => $v['row']['columns'][14],
+            'notice_code' => $v['row']['columns'][15],
+            'iud' => $v['row']['columns'][16],
+            'iuv' => $v['row']['columns'][17],
+            'split' => [],
           ],
-          'online_payment_landing' => [
-            'url' => $responseData[1]['row']['columns'][7],
-            'method' => $responseData[1]['row']['columns'][8],
+          'links' => [
+              'online_payment_begin' => [
+                'url' => $v['row']['columns'][18],
+                'last_opened_at' => $v['row']['columns'][19],
+                'method' => $v['row']['columns'][20],
+              ],
+              'online_payment_landing' => [
+                'url' => $v['row']['columns'][21],
+                'last_opened_at' => $v['row']['columns'][22],
+                'method' => $v['row']['columns'][23],
+              ],
+              'offline_payment' => [
+                'url' => $v['row']['columns'][24],
+                'last_opened_at' => $v['row']['columns'][25],
+                'method' => $v['row']['columns'][26],
+              ],
+              'receipt' => [
+                'url' => $v['row']['columns'][27],
+                'last_opened_at' => $v['row']['columns'][28],
+                'method' => $v['row']['columns'][29],
+              ],
+              'notify' => [],
+              'update' => [
+                'url' => $v['row']['columns'][30],
+                'method' => $v['row']['columns'][31],
+                'last_check_at' => $v['row']['columns'][32],
+                'next_check_at' => $v['row']['columns'][33],
+              ],
+            ],
+          'payer' => [
+            'type' => $v['row']['columns'][34],
+            'tax_identification_number' => $v['row']['columns'][35],
+            'name' => $v['row']['columns'][36],
+            'family_name' => $v['row']['columns'][37],
+            'street_name' => $v['row']['columns'][38],
+            'building_number' => $v['row']['columns'][39],
+            'postal_code' => $v['row']['columns'][40],
+            'town_name' => $v['row']['columns'][41],
+            'country_subdivision' => $v['row']['columns'][42],
+            'country' => $v['row']['columns'][43],
+            'email' => $v['row']['columns'][44],
           ],
-          'offline_payment' => [
-            'url' => $responseData[1]['row']['columns'][9],
-            'method' => $responseData[1]['row']['columns'][10],
-          ],
+          'event_id' => $v['row']['columns'][45],
+          'event_version' => $v['row']['columns'][46],
+          'event_created_at' => $v['row']['columns'][47],
+          'app_id' => $v['row']['columns'][48],
         ];
+        $data []= $payment;
       }
+
       return $data;
     }
   }
