16f41f0b9d1f1b7c59c76b42e2194303bd4d468d
Sabato <sabatogalasso90@gmail.com>
2022-06-28 07:10:03 +0000

    implemented formio calendar translations

---
+++ assets/js/translations/i18n-datepicker.js
@@ -0,0 +1,33 @@
+export const i18nDatepicker = {
+  "it": {
+    "closeText": "Chiudi",
+    "prevText": "",
+    "nextText": "",
+    "currentText": "Oggi",
+    "monthNames": ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+    "monthNamesShort": ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+    "dayNames": ['Domenica', 'Luned&#236', 'Marted&#236', 'Mercoled&#236', 'Gioved&#236', 'Venerd&#236', 'Sabato'],
+    "dayNamesShort": ['Dom', 'Lun', 'Mar', 'Mer', 'Gio', 'Ven', 'Sab'],
+    "dayNamesMin": ['Do', 'Lu', 'Ma', 'Me', 'Gio', 'Ve', 'Sa']
+  }, "en": {
+    "closeText": "Done",
+    "prevText": "",
+    "nextText": "",
+    "currentText": "Today",
+    "monthNames": ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    "monthNamesShort": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    "dayNames": ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    "dayNamesShort": ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    "dayNamesMin": ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+  }, "de": {
+    "closeText": "Schließen",
+    "prevText": "",
+    "nextText": "",
+    "currentText": "Heute",
+    "monthNames": ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    "monthNamesShort": ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
+    "dayNames": ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'],
+    "dayNamesShort": ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+    "dayNamesMin": ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
+  }
+}
