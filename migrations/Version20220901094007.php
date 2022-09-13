1280c91a60edd64bdc712320ce4d7f02efefe5aa
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-09-13 10:10:40 +0200

    Release 2.0.1-rc.6

--- migrations/Version20220901094007.php
+++ migrations/Version20220901094007.php
@@ -20,8 +20,8 @@ final class Version20220901094007 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE servizio ALTER topics SET NOT NULL');
         $this->addSql('UPDATE servizio SET topics = (SELECT id FROM categoria ORDER BY name ASC LIMIT 1) WHERE topics IS null');
+        $this->addSql('ALTER TABLE servizio ALTER topics SET NOT NULL');
     }
 
     public function down(Schema $schema): void
