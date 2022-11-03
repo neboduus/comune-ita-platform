dedf98a2fb97d978e85ade0fdfe3f6b7973d4764
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-11-03 17:08:09 +0100

    Fixed mime type file on request integration, added mime type on allowed mime type list

--- src/Utils/UploadedBase64File.php
+++ src/Utils/UploadedBase64File.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class UploadedBase64File extends UploadedFile
 {
 
-  public function __construct(string $base64Content, $mimeType)
+  public function __construct(string $base64Content, $mimeType, $fileName = null)
   {
     preg_match('/data:([^;]*);base64,(.*)/', $base64Content, $matches);
     if (isset($matches[1])) {
@@ -22,10 +22,12 @@ class UploadedBase64File extends UploadedFile
     fwrite($file, $base64Content);
     $meta_data = stream_get_meta_data($file);
     $path = $meta_data['uri'];
-    $fileName = basename($path);
+    if (empty($fileName)) {
+      $fileName = basename($path);
+    }
     fclose($file);
 
-    parent::__construct($path, $fileName, $mimeType, null, null, true);
+    parent::__construct($path, $fileName, $mimeType, null, true);
   }
 
 }
