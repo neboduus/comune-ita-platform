4494ca37ee8945e088a25350f25f7fd84778679b
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-12-06 12:07:24 +0100

    Release 2.6.0-rc.6

---
+++ src/Form/Admin/ContactPointType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form\Admin;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactPointType extends AbstractType
+{
+
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+      ->add('name', TextType::class, [
+        'required' => true,
+      ])
+      ->add('email', EmailType::class, [
+        'label' => 'Email',
+        'required' => false,
+      ])
+      ->add('url', UrlType::class, [
+        'label' => 'Url',
+        'required' => false,
+      ])
+      ->add('phoneNumber', TextType::class, [
+        'label' => 'contact_point.phone_number',
+        'required' => false,
+      ])
+      ->add('pec', EmailType::class, [
+        'label' => 'Pec',
+        'required' => false,
+      ]);
+  }
+
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults(array(
+      'data_class' => 'App\Entity\ContactPoint',
+    ));
+  }
+}
