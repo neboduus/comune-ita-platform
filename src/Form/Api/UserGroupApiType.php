84d7cfe8bbb0398aa325ba4cfbca5eea6e025ebd
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-12-07 15:24:19 +0100

    Release 2.6.1

--- src/Form/Api/UserGroupApiType.php
+++ src/Form/Api/UserGroupApiType.php
@@ -3,7 +3,6 @@
 namespace App\Form\Api;
 
 use App\Entity\UserGroup;
-use App\Form\Admin\ContactPointApiType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
