3dc3484169a6500a8cba9c14c5bf3d23812fda3e
sabato-galasso <sabatogalasso90@gmail.com>
2022-12-22 16:58:56 +0100

    fix translations

--- assets/app/js/components/message_attachments.js
+++ assets/app/js/components/message_attachments.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-Vue.component('message_attachments', {
+Vue.component("message_attachments", {
   template: `<div>
       <el-upload
         class="vue-upload pb-0"
@@ -10,24 +10,24 @@ Vue.component('message_attachments', {
         :on-error="onError"
         :before-upload="onBeforeUpload"
         :multiple="true">
-        <el-button type="primary">${Translator.trans('pratica.upload_attachment', {}, 'messages', 'it')}</el-button>
+        <el-button type="primary">Carica allegato</el-button>
       </el-upload>
     </div>`,
   data: function () {
-    return vueBundledDataMessage
+    return vueBundledDataMessage;
   },
   computed: {},
   created: function () {
-    this.updateFormValue()
+    this.updateFormValue();
   },
   methods: {
     onSuccess(response, file, fileList) {
       this.attachments.push({
         id: response.id,
         name: response.name,
-        type: response.type
-      })
-      this.updateFormValue()
+        type: response.type,
+      });
+      this.updateFormValue();
     },
     onError(response, file, fileList) {
       alert(response);
@@ -35,24 +35,27 @@ Vue.component('message_attachments', {
     },
     onRemove(file, fileList) {
       if (!file.id && file.response) {
-        file.id = file.response.id
+        file.id = file.response.id;
       }
       for (var o = 0; o < this.attachments.length; o++) {
         if (this.attachments[o].id === file.id) {
           this.attachments.splice(o, 1);
         }
       }
-      this.updateFormValue()
+      this.updateFormValue();
     },
     onBeforeUpload(file) {
       if (file.size / 1024 / 1024 > 25) {
-        this.$message.error(`${Translator.trans('pratica.upload_warning', {}, 'messages', 'it')}`);
+        this.$message.error(
+          "Attenzione: non è possibile caricare file con una dimensione maggiore di 25Mb."
+        );
         return false;
       }
     },
     updateFormValue() {
-      document.getElementById('message_attachments').value = JSON.stringify(this.attachments)
-    }
-  }
-
-})
+      document.getElementById("message_attachments").value = JSON.stringify(
+        this.attachments
+      );
+    },
+  },
+});
--- assets/app/js/components/pratiche-list.js
+++ assets/app/js/components/pratiche-list.js
@@ -1,7 +1,7 @@
 "use strict";
 
-Vue.component('pratiche-list', {
-    template: `<div>
+Vue.component("pratiche-list", {
+  template: `<div>
         <el-dialog
                 title="Action result"
                 v-model="dialogVisible"
@@ -36,78 +36,78 @@ Vue.component('pratiche-list', {
             Bind {{ chosenOperatore }} to ente {{ chosenEnte }} ? <el-button v-bind:loading="pendingBindingOperation" type="primary" v-on:click="submitOperatoreBinding">Save Changes</el-button>
         </div>
     </div>`,
-    data: function () {
-        return {
-            enti: null,
-            operatori: null,
-            chosenOperatore: null,
-            chosenEnte: null,
-            chosenEnteName: null,
-            pendingBindingOperation: false,
-            dialogVisible: false,
-            dialogMessage: null,
-        }
+  data: function () {
+    return {
+      enti: null,
+      operatori: null,
+      chosenOperatore: null,
+      chosenEnte: null,
+      chosenEnteName: null,
+      pendingBindingOperation: false,
+      dialogVisible: false,
+      dialogMessage: null,
+    };
+  },
+  computed: {
+    showOperatoreBindAction: function () {
+      return this.chosenEnte && this.chosenOperatore;
     },
-    computed: {
-        showOperatoreBindAction: function () {
-            return this.chosenEnte && this.chosenOperatore;
-        }
+  },
+  created: function () {
+    this.loadEntities();
+  },
+  methods: {
+    submitOperatoreBinding: function (e) {
+      const self = this;
+      self.pendingBindingOperation = true;
+      axios
+        .post(
+          `/amministrazione/api/operatori/${self.chosenOperatore}/ente/${self.chosenEnte}`
+        )
+        .then(function () {
+          self.operatori = self.operatori.map((item) => {
+            if (item.id === self.chosenOperatore) {
+              item.ente = self.chosenEnte;
+              self.enti.map((e) => {
+                if (e.id === item.ente) {
+                  item.enteName = e.name;
+                }
+              });
+            }
+            return renderOperatoreSelectItem(item);
+          });
+          self.operatori.splice(0, 0); //trigger update
+          self.chosenOperatore = null;
+          self.chosenEnte = null;
+          self.pendingBindingOperation = false;
+          self.dialogVisible = true;
+          self.dialogMessage = "Update performed";
+        })
+        .catch((e) => {
+          self.pendingBindingOperation = false;
+          self.dialogVisible = true;
+          self.dialogMessage = e.message;
+        });
     },
-    created: function() {
-        this.loadEntities();
-    },
-    methods: {
-        submitOperatoreBinding: function (e) {
-            const self = this;
-            self.pendingBindingOperation = true;
-            axios.post(`/amministrazione/api/operatori/${self.chosenOperatore}/ente/${self.chosenEnte}`)
-                .then(function () {
-                    self.operatori = self.operatori.map((item) => {
-                        if (item.id === self.chosenOperatore) {
-                            item.ente = self.chosenEnte;
-                            self.enti.map((e) => {
-                                if (e.id === item.ente) {
-                                    item.enteName = e.name;
-                                }
-                            })
-                        }
-                        return renderOperatoreSelectItem(item);
-                    });
-                    self.operatori.splice(0, 0); //trigger update
-                    self.chosenOperatore = null;
-                    self.chosenEnte = null;
-                    self.pendingBindingOperation = false;
-                    self.dialogVisible = true;
-                    self.dialogMessage = "Update performed";
-                })
-                .catch((e) => {
-                    self.pendingBindingOperation = false;
-                    self.dialogVisible = true;
-                    self.dialogMessage = e.message;
-                })
-        },
-        loadEntities: function () {
-            const self = this;
-            axios.get('/amministrazione/api/operatori')
-                .then(function (response) {
-                    if (response.status == 200) {
-                        self.operatori = response.data.map((item) => {
-                            return renderOperatoreSelectItem(item);
-                        });
-                    }
-                })
-            axios.get('/amministrazione/api/enti')
-                .then(function (response) {
-                    if (response.status == 200) {
-                        self.enti = response.data;
-                    }
-                })
-
+    loadEntities: function () {
+      const self = this;
+      axios.get("/amministrazione/api/operatori").then(function (response) {
+        if (response.status == 200) {
+          self.operatori = response.data.map((item) => {
+            return renderOperatoreSelectItem(item);
+          });
         }
-    }
+      });
+      axios.get("/amministrazione/api/enti").then(function (response) {
+        if (response.status == 200) {
+          self.enti = response.data;
+        }
+      });
+    },
+  },
 });
-const renderOperatoreSelectItem= function(item) {
-  const language = document.documentElement.lang.toString();
-    item.label = item.fullName + (item.ente ? ` (${item.enteName})` : ` (${Translator.trans('login_type.none', {}, 'messages', language)})`);
-    return item;
-}
+const renderOperatoreSelectItem = function (item) {
+  item.label =
+    item.fullName + (item.ente ? ` (${item.enteName})` : ` (nessuno)`);
+  return item;
+};
