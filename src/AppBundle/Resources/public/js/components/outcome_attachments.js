2deffeba6435b6883ffa26f0b386b1d2f1b4a7d0
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2022-12-27 16:08:47 +0100

    Remove translator form vue components

--- src/AppBundle/Resources/public/js/components/outcome_attachments.js
+++ src/AppBundle/Resources/public/js/components/outcome_attachments.js
@@ -1,5 +1,5 @@
-'use strict';
-Vue.component('outcome_attachments', {
+"use strict";
+Vue.component("outcome_attachments", {
   template: `<div>
       <el-upload
         class="vue-upload pb-0"
@@ -9,24 +9,24 @@ Vue.component('outcome_attachments', {
         :on-error="onError"
         :before-upload="onBeforeUpload"
         :multiple="true">
-        <el-button type="primary">${Translator.trans('pratica.upload_attachment', {}, 'messages', 'it')}</el-button>
+        <el-button type="primary">Carica allegato</el-button>
       </el-upload>
     </div>`,
   data: function () {
-    return vueBundledData
+    return vueBundledData;
   },
   computed: {},
   created: function () {
-    this.updateFormValue()
+    this.updateFormValue();
   },
   methods: {
     onSuccess(response, file, fileList) {
       this.attachments.push({
         id: response.id,
         name: response.name,
-        type: response.type
-      })
-      this.updateFormValue()
+        type: response.type,
+      });
+      this.updateFormValue();
     },
     onError(response, file, fileList) {
       alert(response);
@@ -34,23 +34,27 @@ Vue.component('outcome_attachments', {
     },
     onRemove(file, fileList) {
       if (!file.id && file.response) {
-        file.id = file.response.id
+        file.id = file.response.id;
       }
       for (var o = 0; o < this.attachments.length; o++) {
         if (this.attachments[o].id === file.id) {
           this.attachments.splice(o, 1);
         }
       }
-      this.updateFormValue()
+      this.updateFormValue();
     },
     onBeforeUpload(file) {
       if (file.size / 1024 / 1024 > 25) {
-        this.$message.error(`${Translator.trans('pratica.upload_warning', {}, 'messages', 'it')}`);
+        this.$message.error(
+          "Attenzione: non è possibile caricare file con una dimensione maggiore di 25Mb."
+        );
         return false;
       }
     },
     updateFormValue() {
-      document.getElementById('outcome_attachments').value = JSON.stringify(this.attachments)
-    }
-  }
-})
+      document.getElementById("outcome_attachments").value = JSON.stringify(
+        this.attachments
+      );
+    },
+  },
+});
