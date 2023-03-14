65e77eabcf143023d0c021422e0cb15f931ca5a4
Raffaele Luccisano <raffaele.luccisano@gmail.com>
2023-03-14 11:03:11 +0000

    Added default to update topics query for Giscom services

--- migrations/Version20230307095232.php
+++ migrations/Version20230307095232.php
@@ -20,7 +20,7 @@ final class Version20230307095232 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql("UPDATE servizio SET topics = (SELECT id FROM categoria WHERE slug = 'catasto-e-urbanistica' LIMIT 1)
+        $this->addSql("UPDATE servizio SET topics = COALESCE((SELECT id FROM categoria WHERE slug = 'catasto-e-urbanistica' LIMIT 1), (SELECT id FROM categoria LIMIT 1))
                            WHERE slug IN ( 'autorizzazione-paesaggistica-sindaco', 'comunicazione-inizio-lavori', 'comunicazione-inizio-lavori-asseverata',
                                            'domanda-permesso-di-costruire', 'dichiarazione-ultimazione-lavori', 'comunicazione-opere-libere',
                                            'domanda-permesso-di-costruire-in-sanatoria', 'scia-pratica-edilizia','segnalazione-certificata-di-agibilita')");
